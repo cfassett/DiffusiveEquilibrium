7c84a81f74bd1d44a251e84aa5741e1517ff49e5
cfassett <cfassett@gmail.com>
2022-06-23 13:32:21 -0500

    Add files via upload

---
+++ Fig1-equilibrium_ages/scalingparams.py
@@ -0,0 +1,18 @@
+from math import *
+
+
+gravity=1.62        # m/s2
+strength=1.0e4     # Pa
+targdensity=1500.0  #kg/m3 (rho)
+impdensity=2500.0   #kg/m3 (delta)
+velocity=20000.0    # m/s    COULD choose a distribution?   
+alpha=45.0          # impact angle degrees
+effvelocity=velocity*sin(radians(alpha))
+nu=(1.0/3.0)           # ~1/3 to 0.4
+mu=0.41             # ~0.4 to 0.55    Varying mu makes a big difference in scaling, 0.41 from Williams et al. would predict lower fluxes / longer equilibrium times and a discontinuity with Neukum
+K1=0.132
+K2=0.26  
+Kr=1.1*1.3  #Kr + KrRim  
+densityratio=(targdensity/impdensity)
+
+surfacearea = 1.0 #km2
